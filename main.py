e1a3a3b25d49fdf0ce41732279bee3ce39cb5d01
migenteplasticos <migenteplasticos@gmail.com>
2023-02-01 23:21:28 -0400

    add pip

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ import os
 import pymysql
 from flask import Flask, render_template, request, redirect, url_for, session
 
-# connection = MySQLdb.connect(
-#   host= os.getenv("HOST"),
-#   user=os.getenv("USERNAME"),
-#   passwd= os.getenv("PASSWORD"),
-#   db= os.getenv("DATABASE"),
-#   ssl_mode = "VERIFY_IDENTITY",
-#   ssl      = {
-#     "ca": "/etc/ssl/cert.pem"
-#   }
-# )
-
 app = Flask(__name__)
 #secret key
 app.secret_key = 'novaglez'
